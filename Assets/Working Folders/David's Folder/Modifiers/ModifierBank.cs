d2f4034f74a9c02833207ebf1210095fcd1bc480
DavidHamon <davidhamon3@gmail.com>
2025-11-19 21:54:55 +0100

    feat : Event Manager working feat : Event Data Structure done

---
+++ Assets/Working Folders/David's Folder/Modifiers/ModifierBank.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+[System.Serializable]
+public class ModifierBank
+{
+    public Modifier ClimateModifier;
+    public Modifier SocietalModifier;
+    public Modifier TrustModifier;
+}
