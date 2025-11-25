6788a017eb527267b90a2cc1984b1c55b4fcc7b5
Nicolas Rabault <nicolas.rabault@luos.io>
2025-11-25 16:58:01 +0100

    Add humanity collapsing events and calibrate it.

--- Assets/Scripts/Gauges/Human.cs
+++ Assets/Scripts/Gauges/Human.cs
@@ -28,7 +28,7 @@ public class Human : Singleton<Human>
         HumanCount = (long)Mathf.Floor(humanParameter.StartValue);
         floatHumanValue = humanParameter.StartValue;
         humanModifierManager = new ModifierManager();
-        humanModifierManager.modifierScale = humanParameter.HumanModifierScale;
+        humanModifierManager.modifierScale = 1;
 
         // Subscribe to the OnTick event from Timeline
         if (Timeline.Instance != null)
@@ -48,9 +48,10 @@ public class Human : Singleton<Human>
     public void OnTimelineTick(uint currentTick)
     {
         // Population growth is exponential and is calculated monthly
-        float ModifiedGrowth = humanParameter.PopulationGrowthPerYear + humanModifierManager.ComputeModifierValue();
+        float modifierValue = humanModifierManager.ComputeModifierValue();
+        float ModifiedGrowth = humanParameter.PopulationGrowthPerYear + (modifierValue / 100.0f);
         float TickGrowth = ModifiedGrowth / (12 / Timeline.Instance.tickDuration);
-        float humanGrowth = floatHumanValue * TickGrowth;
+        float humanGrowth = (floatHumanValue * TickGrowth) + (modifierValue * humanParameter.HumanModifierScale);
         PopulationDelta = (long)Mathf.Floor(humanGrowth);
         floatHumanValue += humanGrowth;
 
@@ -65,6 +66,10 @@ public class Human : Singleton<Human>
     public void AddModifier(Modifier modifier)
     {
         humanModifierManager.AddModifier(modifier);
-        floatHumanValue += modifier.OneShotValue;
+        if (modifier.OneShotValue != 0)
+        {
+            Debug.Log("OneShotValue: are not supported for human");
+            return;
+        }
     }
 }
