567f9c40606042203210e8710d8b3e3c027f5ee4
NicoCcino <frangins_freeman@hotmail.fr>
2025-11-26 18:16:57 +0100

    Functional full boot flow

--- Assets/Working Folders/Nicolas F's Folder/Loading.cs
+++ Assets/Working Folders/Nicolas F's Folder/Loading.cs
@@ -3,6 +3,7 @@ using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 using System.Collections;
 using TMPro;
+using WiDiD.UI;
 
 public class Loading : MonoBehaviour
 {
@@ -10,28 +11,53 @@ public class Loading : MonoBehaviour
     public TextMeshProUGUI progressText;
     public float updateTextDelay = 0.3f;
     public float minimumLoadTime = 2f;
+    public CanvasGroupCustom canvasGroupCustom;
+    public TextMeshProUGUI textDescription;
 
     public string sceneToLoad;
 
     private string[] dots = new string[] { ".", "..", "..." };
     private int dotIndex = 0;
+    // Async load variables
+    private AsyncOperation asyncLoad;
+    private Coroutine textUpdateCoroutine;
+    private float timer = 0f;
 
     void Start()
     {
+        textUpdateCoroutine = StartCoroutine(UpdateLoadingText());
         StartCoroutine(LoadAsync());
-        StartCoroutine(UpdateLoadingText());
+
     }
 
+    public void DisplayCreatorPrompt()
+    {
+        canvasGroupCustom.Fade(true);
+        var typewriterEffect = textDescription.GetComponent<TypewriterEffect>();
+        Debug.Log("Playing typewriter effet with this text: " + textDescription.text);
+        typewriterEffect.Play(textDescription.text);
+    }
+
+    public void StartGame()
+    {
+        if (asyncLoad.progress >= 0.9f && timer >= minimumLoadTime)
+        {
+            Debug.Log("Game will now start.");
+            asyncLoad.allowSceneActivation = true;
+        }
+    }
     IEnumerator LoadAsync()
     {
-        float timer = 0f;
+        timer = 0f;
 
-        AsyncOperation operation = SceneManager.LoadSceneAsync(sceneToLoad);
-        operation.allowSceneActivation = false;
+        bool creatorPromptDisplayed = false;
 
-        while (!operation.isDone)
+        asyncLoad = SceneManager.LoadSceneAsync(sceneToLoad);
+        asyncLoad.allowSceneActivation = false;
+
+        while (!asyncLoad.isDone)
         {
-            float progress = Mathf.Clamp01(operation.progress / 0.9f);
+            float progress = Mathf.Clamp01(asyncLoad.progress / 0.9f);
 
             // Optionnel : Afficher le pourcentage (à côté des points)
             // progressText.text = $"{Mathf.RoundToInt(progress * 100f)}% {dots[dotIndex]}";
@@ -40,10 +66,21 @@ public class Loading : MonoBehaviour
 
             timer += Time.deltaTime;
 
-            // Quand tout est prêt ET le délai minimum est passé
-            if (operation.progress >= 0.9f && timer >= minimumLoadTime)
+            // Quand le délai minimum est passé
+            if (timer >= minimumLoadTime && !creatorPromptDisplayed)
             {
-                operation.allowSceneActivation = true;
+                Debug.Log("Minimum time OK. Now displaying creator prompt and stopping text update coroutine.");
+                creatorPromptDisplayed = true;
+                DisplayCreatorPrompt();
+                StopCoroutine(textUpdateCoroutine);
+            }
+
+            // Quand tout est prêt ET le délai minimum est passé
+            if (asyncLoad.progress >= 0.9f && timer >= minimumLoadTime)
+            {
+                // Debug.Log("Game loaded and ready to start.");
+                StopCoroutine(UpdateLoadingText());
+                // operation.allowSceneActivation = true;
             }
 
             yield return null;
@@ -54,7 +91,10 @@ public class Loading : MonoBehaviour
     {
         while (true)
         {
-            dotIndex = (dotIndex + 1) % dots.Length;
+            if (!canvasGroupCustom.IsVisible)
+            {
+                dotIndex = (dotIndex + 1) % dots.Length;
+            }
             yield return new WaitForSeconds(updateTextDelay);
         }
     }
