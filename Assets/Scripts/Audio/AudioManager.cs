61fa915908ddfa8c377dc9abac4a9555a36ef914
NicoCcino <frangins_freeman@hotmail.fr>
2025-11-28 19:30:39 +0100

    Hotfix win/lose

--- Assets/Scripts/Audio/AudioManager.cs
+++ Assets/Scripts/Audio/AudioManager.cs
@@ -5,7 +5,7 @@ public class AudioManager : Singleton<AudioManager>
     [SerializeField] private AudioSource audioSource;
     public void PlaySound(AudioClip audioClip, float volume)
     {
-        if (audioClip != null)
+        if (audioClip != null && audioSource != null)
         {
             audioSource.PlayOneShot(audioClip, volume);
         }
